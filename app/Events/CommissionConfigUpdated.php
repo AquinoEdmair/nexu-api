cfad30f43b3dc54226082ba593ae560b8aeef6da
Edmair Antonio Aquino <aquinoedmair@outlook.com>
2026-04-11 19:36:07 -0600

    fix: add Dispatchable trait to CommissionConfigUpdated event to support static dispatching

--- app/Events/CommissionConfigUpdated.php
+++ app/Events/CommissionConfigUpdated.php
@@ -7,8 +7,14 @@ namespace App\Events;
 use App\Models\Admin;
 use App\Models\CommissionConfig;
 
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\InteractsWithSockets;
+
 final class CommissionConfigUpdated
 {
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
     public function __construct(
         public readonly CommissionConfig  $config,
         public readonly Admin             $admin,
