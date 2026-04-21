cc81606dbe1ccb5ab7d540c90719f03d47dd52db
Edmair Antonio Aquino <aquinoedmair@outlook.com>
2026-04-20 22:54:21 -0600

    fix: remove duplicate event listeners to stop notification duplication

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -67,21 +67,7 @@ class AppServiceProvider extends ServiceProvider
         Gate::policy(WithdrawalRequest::class, WithdrawalRequestPolicy::class);
         Gate::policy(CommissionConfig::class, CommissionConfigPolicy::class);
 
-        Event::listen(\App\Events\DepositInitiated::class, \App\Listeners\NotifyAdminOnDepositInitiated::class);
         Event::listen(UserCreatedByAdmin::class, SendWelcomeEmailToNewUser::class);
-        Event::listen(UserStatusChanged::class, NotifyUserOnStatusChange::class);
-        Event::listen(YieldApplied::class, NotifyUsersOnYieldApplied::class);
-        Event::listen(YieldApplied::class, NotifyAdminOnYieldCompleted::class);
-        Event::listen(WithdrawalApproved::class, NotifyUserWithdrawalApproved::class);
-        Event::listen(WithdrawalRejected::class, NotifyUserWithdrawalRejected::class);
-        Event::listen(CommissionConfigUpdated::class, LogCommissionConfigChange::class);
-        Event::listen(DepositConfirmed::class, NotifyUserOnDeposit::class);
-        Event::listen(DepositConfirmed::class, ProcessReferralOnDeposit::class);
-        Event::listen(DepositConfirmed::class, NotifyAdminOnDeposit::class);
-        Event::listen(YieldApplied::class, AwardPointsOnYield::class);
-        Event::listen(UserRegisteredWithReferral::class, NotifyReferrerOnNewSignup::class);
-        Event::listen(WithdrawalRequested::class, NotifyAdminOnWithdrawalRequested::class);
-        Event::listen(TicketCreated::class, NotifyAdminOnTicketCreated::class);
         Event::listen(\Illuminate\Auth\Events\Registered::class, NotifyAdminOnNewUser::class);
     }
 }
